33771d1cfacc8b63e73959420dcd37317918e75a
gabriel <elias@fortalezatec.com.br>
2021-09-17 16:02:02 -0300

    Finalizado Totalmente

--- App.js
+++ App.js
@@ -5,21 +5,21 @@ import CurrentPrice from './src/components/CurrentPrice';
 import HistoryGraphic from './src/components/HistoryGraphic';
 import QuotationsList from './src/components/QuotationsList';
 
-function addZero(number){
-  if(number <= 9){
+function addZero(number) {
+  if (number <= 9) {
     return "0" + number
   }
   return number
 }
 
-function url(qtdDays){
+function url(qtdDays) {
   const date = new Date();
   const listLastDays = qtdDays;
-  const end_date = 
-  `${date.getFullYear()}-${addZero(date.getMonth()+1)}-${addZero(date.getDate())}`
+  const end_date =
+    `${date.getFullYear()}-${addZero(date.getMonth() + 1)}-${addZero(date.getDate())}`
   date.setDate(date.getDate() - listLastDays)
   const start_date =
-  `${date.getFullYear()}-${addZero(date.getMonth()+1)}-${addZero(date.getDate())}`
+    `${date.getFullYear()}-${addZero(date.getMonth() + 1)}-${addZero(date.getDate())}`
   return `https://api.coindesk.com/v1/bpi/historical/close.json?start=${start_date}&end=${end_date}`
 }
 
@@ -27,10 +27,11 @@ async function getPriceCoinsGraphic(url) {
   let responseG = await fetch(url)
   let returnApiG = await responseG.json()
   let selectListQuotationsG = returnApiG.bpi
-  const queryCoinsList = Object.keys(selectListQuotationsG).map((key) => {
-      selectListQuotationsG[key]
+  
+  const queryCoinsListG = Object.keys(selectListQuotationsG).map((key) => {
+    return selectListQuotationsG[key];
   });
-  let dataG = queryCoinsList;
+  let dataG = queryCoinsListG;
   return dataG;
 }
 
@@ -40,8 +41,8 @@ async function getListCoins(url) {
   let selectListQuotations = returnApi.bpi
   const queryCoinsList = Object.keys(selectListQuotations).map((key) => {
     return {
-    data: key.split("-").reverse().join("/"),
-    valor: selectListQuotations[key]
+      data: key.split("-").reverse().join("/"),
+      valor: selectListQuotations[key]
     };
   });
   let data = queryCoinsList.reverse();
@@ -56,36 +57,38 @@ export default function App() {
   const [updateData, setUpdateData] = useState(true);
   const [price, setPrice] = useState()
 
-  function updateDay(number){
+  function updateDay(number) {
     setDays(number);
     setUpdateData(true)
   }
 
-  function priceQuotation(){
+  function priceQuotation() {
     setPrice(coinsGraphicList.pop())
   }
 
-  useEffect(() =>{
-    getListCoins(url(days)).then((data) =>{
+  useEffect(() => {
+    getListCoins(url(days)).then((data) => {
       setCoinsList(data)
     });
-    getPriceCoinsGraphic(url(days)).then((dataG) =>{
-      setCoinsGraphicList(dataG)
+    getPriceCoinsGraphic(url(days)).then((dataG) => {
+      if (dataG != undefined) {
+        setCoinsGraphicList(dataG)
+      }
     });
     priceQuotation()
-    if(updateData){
+    if (updateData) {
       setUpdateData(false)
     }
-  },[updateData]);
+  }, [updateData]);
 
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar 
-      backgroundColor="#f50d41"
-      barStyle="dark-content"
+      <StatusBar
+        backgroundColor="#f50d41"
+        barStyle="dark-content"
       />
-      <CurrentPrice lastQuotation={price}/>
-      <HistoryGraphic infoDataGraphic={coinsGraphicList}/>
+      <CurrentPrice lastQuotation={price} />
+      <HistoryGraphic infoDataGraphic={coinsGraphicList} />
       <QuotationsList filterDay={updateDay} listTransactions={coinsList} />
     </SafeAreaView>
   );
@@ -96,6 +99,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#000000',
     alignItems: 'center',
-    paddingTop: Platform.OS === "android" ? 40: 0
+    paddingTop: Platform.OS === "android" ? 40 : 0
   },
 });
